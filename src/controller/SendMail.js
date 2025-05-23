25562e21ab10d9239d891b0879464be241d6670e
Renan Freitas <renan.freitas.anjos@gmail.com>
2025-05-23 00:00:39 -0300

    Refactor: connect in new repository

--- src/controller/SendMail.js
+++ src/controller/SendMail.js
@@ -2,11 +2,19 @@ const emailjs = require("@emailjs/browser")
 const axios = require("axios")
 
 async function init(){
-    
+    await emailjs.init({
+        publicKey: process.env.EMAILJS_PUBLIC_KEY,
+    });
 }
 
 async function SendMail(chatId, data, client) {
-    const
+    const response = await axios.post("https://api.emailjs.com/api/v1.0/email/send", data, {
+        headers: {
+            "Content-Type": "application/json" ,
+            "Authorization": process.env.EMAILJS_PRIVATE_KEY,
+        },
+    }).then(res => client.sendMessage(chatId,"Is over"))
+    .catch(err => client.sendMessage(chatId, "Falha ao carregar"))
 }
 
 module.exports = {
